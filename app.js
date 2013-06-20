b18380033e0d0c78e5e3b0e9611b4d72b73e2db1
Simone Lusenti <simone@plasticpanda.com>
2013-06-20 16:08:35 +0200

    Working

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 
 var fs = require('fs')
   , clog = require('clog')
+  , async = require('async')
+  , random = require('node-random')
   , http = require('http')
   , ApollonianGasket = require('./lib/ApollonianGasket');
 
@@ -13,30 +15,54 @@ require('sugar');
 function generate() {
   var palette = ['#423A38', '#47B8C8', '#E7EEE2', '#BDB9B1', '#D7503E']
     , canvas
-    , apollo;
+    , apollo
+    , size = 512;
+  
+  async.parallel({
     
+    random: function (done) {
+      random.numbers({
+        number: 1,
+        minimum: Math.round(size * 0.3),
+        maximum: Math.round(size * 0.7)
+      }, done);
+    },
     
-  http.get('http://www.colourlovers.com/api/palettes/random?format=json', function (res) {
-    res.on('data', function (data) {
-      var body = JSON.parse(data.toString('utf8'))
-        , colors = body[0].colors.map(function (row) { return '#' + row; });
+    colors: function (done) {
+      http.get('http://www.colourlovers.com/api/palettes/random?format=json', function (res) {
+        res.on('data', function (data) {
+          var body = JSON.parse(data.toString('utf8'))
+            , colors = body[0].colors.map(function (row) { return '#' + row; });
+            
+          done(null, colors);
+        });
         
-      clog.info('Got colors', colors);
-      
-      apollo = new ApollonianGasket({
-        size: 512
-      , depth: 4
-      , palette: colors
+      }).on('error', function (err) {
+        done(err);
       });
-      
-      canvas = apollo.draw();
-      fs.writeFileSync('index.html', '<html><body><img src="' + canvas.toDataURL() + '" /></body></html>');
-      clog.ok('Wrote file index.html');
+    }
+    
+  }, function _callback(err, data) {
+    if (err) {
+      throw err;
+    }
+    
+    clog.info('Got random', data.random[0]);
+    clog.info('Got colors', data.colors);
+    
+    apollo = new ApollonianGasket({
+      size: size
+    , depth: 4
+    , palette: data.colors
+    , random: data.random[0]
     });
     
-  }).on('error', function (err) {
-    clog.error('Colourlovers API error', err);
+    canvas = apollo.draw();
+    fs.writeFileSync('index.html', '<html><body><img src="' + canvas.toDataURL() + '" /></body></html>');
+    clog.ok('Wrote file index.html');
   });
+  
+    
     
 }
 
