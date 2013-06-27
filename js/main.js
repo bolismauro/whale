1e82404cd1cb3021df24a8527396f960f1849570
Emanuele <emanbrivio@gmail.com>
2013-06-27 17:42:27 +0200

    Initial generate a new avatar

--- js/main.js
+++ js/main.js
@@ -4,6 +4,71 @@ $(function () {
 
   'use strict';
 
+  // Allow Editing Hash type http://www.whale.im/#{mail}|{token}
+  var hash = window.location.hash;
+
+  if (hash) {
+    console.log('Hash', decodeURIComponent(window.location.hash));
+    
+    hash = decodeURIComponent(window.location.hash);
+    hash = hash.replace('#', '').split('|');
+    
+    var mail = hash[0]
+      , token = hash[1];
+
+    console.log('Split', mail, token);
+    
+    /*$.ajax({
+      type: 'GET',
+      url: 'http://api.whale.im/?meta=1&email=' + mail + '&force=' + token,
+      dataType: 'json',
+      success: function(data) {
+        //console.log('Success', data);
+
+        $('.preview img').attr('src', 'data:image/png;base64,' + data.meta.base64);
+        $('.preview input').val(data.url);
+
+        // Generating Palette Bar
+        data.meta.palette.forEach(function (e) {
+          $('<div class="palette-bar" />').css('background-color', e).prependTo($('#result'));
+        });
+
+        // Scroll down!
+        //scrollHandler.mCustomScrollbar('scrollTo', $('#result').position().top);
+        $('#pattern').css('top', -document.body.clientHeight);
+        setTimeout(function () {
+          $('#pattern').remove();
+        }, 800);
+
+
+        $('#result').css('opacity', 1);
+        
+
+        var timer = 800;
+
+        $('.palette-bar').each(function () {
+          var $bar = $(this)
+            , index = $bar.index()
+            , len = $('.palette-bar').size()
+            , t = timer;
+
+          timer = timer - 100;
+
+          setTimeout(function () {      
+            //console.log('bar', $bar, 100 - 100 / len * index + '%');
+            $bar.css('width', 100 - 100 / len * index + '%');
+          }, t);
+        });
+
+      },
+      error: function (data) {
+        throw new Error('Error' + JSON.stringify(data));
+      }
+
+    });*/
+  }
+
+  
   
   // Enable inputs on page load (need this for Firefox)
   $('[type="submit"]').removeAttr('disabled');
@@ -16,15 +81,6 @@ $(function () {
     
     $('[type="submit"]').addClass('loading').attr('disabled', 'disabled');
     $('[type="email"]').attr('disabled', 'disabled');
-
-    // AJAX call to bring Whales to the party!
-    // http://api.whale.im/?email=xxx@xxx.xxx
-    /*$.ajax({
-      url: 'http://api.whale.im/?email=' + $('#mailaddress').val(),
-      context: document.body
-    }).done(function (data) {
-      console.log('data', data);
-    });*/
 
     $.ajax({
       type: 'GET',
